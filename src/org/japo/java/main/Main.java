c8c83b10738b02a45533a7107f35295a143332f1
AdrianDAW <Adriandaw@gmail.com>
2019-11-05 08:33:16 +0100

    :tada: Commit inicial

---
+++ src/org/japo/java/main/Main.java
@@ -0,0 +1,32 @@
+/* 
+ * Copyright 2019 Adrian Bueno Olmedo <adrian.bueno.alum@iescamp.es>.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.japo.java.main;
+
+/**
+ *
+ * @author Adrian Bueno Olmedo <adrian.bueno.alum@iescamp.es>
+ */
+public class Main {
+
+    public static void main(String[] args) {
+        final int cuenta = 9;
+
+        for (int numero = 1; numero <= cuenta; numero++) {
+            System.out.printf("%d%n", numero);
+        }    
+        System.out.println("END");
+    }
+}
